c2ee1bdb5fb2bebd4b84d56cfbede9e7dfd758ae
GURUDEV <gurudev1503l@gmail.com>
2026-08-06 18:47:42 +0530

    Create Bank class

--- main.py
+++ main.py
@@ -1,5 +1,56 @@
-from account import Account
+from bank import Bank
 
-account = Account(1001, "Guru", 5000)
-account.deposit(10000)
-account.display_account()
+bank = Bank()
+
+# -----------------------------
+# Create Accounts
+# -----------------------------
+bank.create_account("Guru", 5000)
+bank.create_account("Rahul", 7000)
+
+# -----------------------------
+# View All Accounts
+# -----------------------------
+print("\n===== VIEW ALL ACCOUNTS =====")
+bank.view_all_accounts()
+
+# -----------------------------
+# Search Account
+# -----------------------------
+print("\n===== SEARCH ACCOUNT =====")
+account = bank.search_account(1001)
+
+if account:
+    account.display_account()
+else:
+    print("Account not found.")
+
+# -----------------------------
+# Deposit
+# -----------------------------
+print("\n===== DEPOSIT =====")
+bank.deposit(1001, 2000)
+
+# -----------------------------
+# Withdraw
+# -----------------------------
+print("\n===== WITHDRAW =====")
+bank.withdraw(1002, 1000)
+
+# -----------------------------
+# Check Balance
+# -----------------------------
+print("\n===== CHECK BALANCE =====")
+bank.check_balance(1001)
+
+# -----------------------------
+# Delete Account
+# -----------------------------
+print("\n===== DELETE ACCOUNT =====")
+bank.delete_account(1002)
+
+# -----------------------------
+# View All Accounts Again
+# -----------------------------
+print("\n===== VIEW ALL ACCOUNTS AFTER DELETE =====")
+bank.view_all_accounts()
